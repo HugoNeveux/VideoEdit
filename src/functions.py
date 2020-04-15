ebba3d72a69c3584f132c96a701b2c314364fd21
HugoNeveux <hugoneveux@orange.fr>
2020-04-15 15:07:53 +0200

    Add time limit for overlay apparition

--- src/functions.py
+++ src/functions.py
@@ -3,15 +3,17 @@ import numpy as np
 from progress.bar import Bar
 import time
 
-def video_overlay(video_path, image_path, begin=0, end=0, output_file='out.avi'):
+def video_overlay(video_path, image_path, begin, end, output_file='out.avi'):
     ########## Reading video ########## 
     cap = cv2.VideoCapture(video_path) # Reading base video
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    framerate = cap.get(cv2.CAP_PROP_FPS)
 
     ########## Read and configure image and masks ##########
     overlay = cv2.imread(image_path, -1)    # Opening overlay
 
     mask = overlay[:,:,3]   # Creating mask
-    mask_inv = cv2.bitwise_not(mask)    # Creating inverted mask
+    mask_inv = cv2.bitwise_not(mask)    # Creating inverted 
 
     overlay = overlay[:,:,0:3]  # PNG conversion to BGR
 
@@ -19,12 +21,17 @@ def video_overlay(video_path, image_path, begin=0, end=0, output_file='out.avi')
 
     ########## Creating VideoWriter object for output ##########
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out = cv2.VideoWriter(output_file[0], fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
+    out = cv2.VideoWriter(output_file[0], fourcc, framerate, (int(cap.get(3)), int(cap.get(4))))
 
     ########## Other ##########
+    frame_counter = 0
     success = True  # Initializing success variable
+    frame_min = int(round(framerate)) * begin 
+    if end != 0:
+        frame_max = int(round(framerate)) * end
+    else:
+        frame_max = frame_count
 
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     bar = Bar('Adding overlay...', max=frame_count) # Creating progress bar 
 
     ########## Main loop ##########
@@ -33,13 +40,15 @@ def video_overlay(video_path, image_path, begin=0, end=0, output_file='out.avi')
         if np.shape(frame) == ():   # If frame isn't empty
             break
         bar.next()  # Bar progress
-        roi = frame[0:rows, 0:cols] 
-        bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
-        fg = cv2.bitwise_and(overlay, overlay, mask = mask)
-        
-        dst = cv2.add(bg, fg)   # Add overlay (fg) to background (bg)
+        frame_counter += 1
+        if frame_min <= frame_counter <= frame_max:
+            roi = frame[0:rows, 0:cols] 
+            bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
+            fg = cv2.bitwise_and(overlay, overlay, mask = mask)
+            
+            dst = cv2.add(bg, fg)   # Add overlay (fg) to background (bg)
 
-        frame[0: rows, 0: cols] = dst
+            frame[0: rows, 0: cols] = dst
         out.write(frame)    # Writing frame with overlay to output
 
     bar.finish()
--- src/main.py
+++ src/main.py
@@ -7,14 +7,14 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-O', '--overlay', nargs=2, type=str, metavar="INPUT_FILES")
-    parser.add_argument('-e', '--end', nargs=1, type=int, metavar='end_time', default=0)
-    parser.add_argument('-b', '--begin', nargs=1, type=int, metavar='begin_time', default=0)
+    parser.add_argument('-e', '--end', nargs=1, type=int, metavar='end_time', default=[0])
+    parser.add_argument('-b', '--begin', nargs=1, type=int, metavar='begin_time', default=[0])
     parser.add_argument('-o', '--output', nargs=1, type=str, metavar="OUTPUT_FILE", default="out.avi")
     args = parser.parse_args()
     
     begin_time = time.time()
     if args.overlay:
-        video_overlay(args.overlay[0], args.overlay[1], args.begin, args.end, args.output)
+        video_overlay(args.overlay[0], args.overlay[1], args.begin[0], args.end[0], args.output)
 
     print(f"Execution finished in {round(time.time() - begin_time)}s")
 
