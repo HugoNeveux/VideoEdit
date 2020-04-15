32ddc407acd964ff37fa862874a5b7d80415b3f2
HugoNeveux <hugoneveux@orange.fr>
2020-04-15 13:14:27 +0200

    Add execution time display

--- src/main.py
+++ src/main.py
@@ -11,9 +11,12 @@ def main():
     parser.add_argument('-b', '--begin', nargs=1, type=int, metavar='begin_time', default=0)
     parser.add_argument('-o', '--output', nargs=1, type=str, metavar="OUTPUT_FILE", default="out.avi")
     args = parser.parse_args()
-
+    
+    begin_time = time.time()
     if args.overlay:
         video_overlay(args.overlay[0], args.overlay[1], args.begin, args.end, args.output)
+
+    print(f"Execution finished in {round(time.time() - begin_time)}s")
 
 
 if __name__ == "__main__":
